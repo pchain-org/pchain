0e8a1c33d9bca2a13af50629890a5799c4429b42
clyde1206 <cybaoz@163.com>
2018-08-08 17:18:14 +0800

    Update test.js

--- test/contracts/test.js
+++ test/contracts/test.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const solc = require('solc')
 const Web3 = require('web3');
 var web3 = new Web3();
-
+//test
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
 
