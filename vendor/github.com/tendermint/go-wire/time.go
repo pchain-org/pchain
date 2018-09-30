d0218c8b7ca146405ace0aaafb17b0e56aecaf9d
Philip Fan <fanwengang@hotmail.com>
2018-09-30 15:01:11 +0800

    deal with the special time

--- vendor/github.com/tendermint/go-wire/time.go
+++ vendor/github.com/tendermint/go-wire/time.go
@@ -13,13 +13,23 @@ This is to ease compatibility with Javascript etc.
 */
 
 func WriteTime(t time.Time, w io.Writer, n *int, err *error) {
-	nanosecs := t.UnixNano()
-	millisecs := nanosecs / 1000000
-	WriteInt64(millisecs*1000000, w, n, err)
+	if t.IsZero() {
+		WriteInt64(0, w, n, err)
+	} else {
+		nanosecs := t.UnixNano()
+		millisecs := nanosecs / 1000000
+		WriteInt64(millisecs*1000000, w, n, err)
+	}
 }
 
 func ReadTime(r io.Reader, n *int, err *error) time.Time {
 	t := ReadInt64(r, n, err)
+	if t == 0 {
+		t_zero := time.Now()
+		t_zero.UnmarshalText([]byte("0001-01-01T00:00:00Z"))
+		return t_zero
+	}
+
 	if t%1000000 != 0 {
 		PanicSanity("Time cannot have sub-millisecond precision")
 	}
