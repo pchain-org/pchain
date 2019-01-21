11834f79eac35ddecca1e885b898179cef493e91
pchain-org <lvhaojin@gmail.com>
2019-01-21 11:46:28 +0800

    make some cli options appliable when the options are at the tail of command

--- cmd/generate_private_validator.go
+++ cmd/generate_private_validator.go
@@ -8,18 +8,22 @@ import (
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/tendermint/go-wire"
 	"gopkg.in/urfave/cli.v1"
+	"os"
 	"path/filepath"
 )
 
 func GeneratePrivateValidatorCmd(ctx *cli.Context) error {
-
-	makeFlagsGlobal(ctx)
 
 	address := ctx.Args().First()
 
 	if address == "" {
 		log.Info("address is empty, need an address")
 		return nil
+	}
+
+	datadir := ctx.GlobalString(utils.DataDirFlag.Name);
+	if err := os.MkdirAll(datadir, 0700); err != nil {
+		return err
 	}
 
 	privValFile := filepath.Join(ctx.GlobalString(utils.DataDirFlag.Name), "priv_validator.json")
