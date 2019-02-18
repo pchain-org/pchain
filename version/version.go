1051cc72a232dd96f896425853efa3f7c9ce9f29
liaoyd <37823194+liaoyd@users.noreply.github.com>
2019-02-18 17:09:02 +0800

    update testnet version

--- version/version.go
+++ version/version.go
@@ -8,7 +8,7 @@ const (
 	Major = 0         // Major version component of the current release
 	Minor = 9         // Minor version component of the current release
 	Patch = 0         // Patch version component of the current release
-	Meta  = "test2.3" // Version metadata to append to the version string
+	Meta  = "test2.4" // Version metadata to append to the version string
 )
 
 var (
