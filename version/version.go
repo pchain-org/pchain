6e0d717db33682a23c188d260c668bad391f1859
Philip Fan <fanwengang@hotmail.com>
2019-05-13 10:42:35 +0800

    Update version to 1.0.18
    Fix save data to main chain failed case

--- version/version.go
+++ version/version.go
@@ -7,7 +7,7 @@ import (
 const (
 	Major = 1  // Major version component of the current release
 	Minor = 0  // Minor version component of the current release
-	Patch = 17 // Patch version component of the current release
+	Patch = 18 // Patch version component of the current release
 	Meta  = "" // Version metadata to append to the version string
 )
 
