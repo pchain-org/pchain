be64b4b1d38c6eda2f9281aa0ec6dbfabab463eb
Philip Fan <fanwengang@hotmail.com>
2019-07-17 14:53:04 +0800

    vendor, crypto, swarm: switch over to upstream sha3 package

--- vendor/github.com/ethereum/go-ethereum/consensus/istanbul/backend/engine.go
+++ vendor/github.com/ethereum/go-ethereum/consensus/istanbul/backend/engine.go
@@ -31,11 +31,11 @@ import (
 	"github.com/ethereum/go-ethereum/consensus/istanbul/validator"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/crypto/sha3"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/ethereum/go-ethereum/rpc"
 	lru "github.com/hashicorp/golang-lru"
+	"golang.org/x/crypto/sha3"
 )
 
 const (
@@ -616,7 +616,7 @@ func (sb *backend) snapshot(chain consensus.ChainReader, number uint64, hash com
 // panics. This is done to avoid accidentally using both forms (signature present
 // or not), which could be abused to produce different hashes for the same header.
 func sigHash(header *types.Header) (hash common.Hash) {
-	hasher := sha3.NewKeccak256()
+	hasher := sha3.NewLegacyKeccak256()
 
 	// Clean seal is required for calculating proposer seal.
 	rlp.Encode(hasher, types.IstanbulFilteredHeader(header, false))
