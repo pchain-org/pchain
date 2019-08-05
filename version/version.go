cf357597930f0ef666536ab678f690e50bff7a5a
liaoyd <37823194+liaoyd@users.noreply.github.com>
2019-08-05 15:45:51 +0800

    update version to 1.0.26

--- version/version.go
+++ version/version.go
@@ -7,7 +7,7 @@ import (
 const (
 	Major = 1  // Major version component of the current release
 	Minor = 0  // Minor version component of the current release
-	Patch = 25 // Patch version component of the current release
+	Patch = 26 // Patch version component of the current release
 	Meta  = "" // Version metadata to append to the version string
 )
 
