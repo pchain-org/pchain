bd272499ae1381d01ffd725b5c8eb898d06420a1
pchain-org <lv@pchain.org>
2019-08-29 18:57:47 +0800

    reduce data-reduction auxiliary storage and add new protection

--- vendor/github.com/ethereum/go-ethereum/core/datareduction/prune_processor.go
+++ vendor/github.com/ethereum/go-ethereum/core/datareduction/prune_processor.go
@@ -13,18 +13,21 @@ import (
 	"github.com/ethereum/go-ethereum/trie"
 	"sort"
 	"sync/atomic"
+	"time"
 )
 
 var (
 	// max scan trie height
 	max_count_trie uint64 = 1000
 	// max retain trie height
-	max_remain_trie uint64 = 10
+	max_remain_trie uint64 = 1000
 	// emptyRoot is the known root hash of an empty trie.
 	emptyRoot = common.HexToHash("56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421")
 
 	pruning int32 // indicate pruning is running or not
 )
+
+type NodeCount map[common.Hash]uint64
 
 type PruneProcessor struct {
 	db      ethdb.Database // Low level persistent database to store prune counting statistics
@@ -33,7 +36,9 @@ type PruneProcessor struct {
 	bc      *core.BlockChain
 	chainDb ethdb.Database // database instance to delete the state/block data
 
-	pendingDeleteHashList []common.Hash // Temp List for batch delete the node
+	pruneBodyData bool
+
+	nodeCount NodeCount
 }
 
 type PruneStatus struct {
@@ -42,8 +47,6 @@ type PruneStatus struct {
 	LatestScanNumber  uint64 `json:"latest_scan_number"`
 	LatestPruneNumber uint64 `json:"latest_prune_number"`
 }
-
-type NodeCount map[common.Hash]uint64
 
 type processLeafTrie func(addr common.Address, account state.Account)
 
@@ -55,85 +58,96 @@ func StopPruning() bool {
 	return atomic.CompareAndSwapInt32(&pruning, 1, 0)
 }
 
-func NewPruneProcessor(chaindb, prunedb ethdb.Database, bc *core.BlockChain) *PruneProcessor {
+func NewPruneProcessor(chaindb, prunedb ethdb.Database, bc *core.BlockChain, pruneBodyData bool) *PruneProcessor {
 	return &PruneProcessor{
 		db:                    prunedb,
 		prunedb:               NewDatabase(prunedb),
 		bc:                    bc,
 		chainDb:               chaindb,
-		pendingDeleteHashList: nil,
+		pruneBodyData:         pruneBodyData,
+		nodeCount:           make(NodeCount),
 	}
 }
 
 func (p *PruneProcessor) Process(blockNumber, scanNumber, pruneNumber uint64) (uint64, uint64) {
-	newScanNumber, newPruneNumber := scanNumber, pruneNumber
 
-	// Step 1. determine the scan height
-	scanOrNot, scanStart, scanEnd := calculateScan(scanNumber, blockNumber)
+	var needScan bool
+	var scanStart, scanEnd uint64
+	for {
+		// Step 1. determine the scan height
+		needScan, scanStart, scanEnd = calculateScan(scanNumber, blockNumber)
 
-	log.Infof("Data Reduction - scan ? %v , %d - %d", scanOrNot, scanStart, scanEnd)
+		log.Infof("Data Reduction - scan ? %v , %d - %d", needScan, scanStart, scanEnd)
 
-	if scanOrNot {
-		// Step 2. Read Latest Node Count
-		nodeCount := p.readLatestNodeCount(scanNumber, pruneNumber)
+		if needScan {
 
-		var stateRoots []common.Hash
-		if pruneNumber > 0 {
-			// Add previous state root for prune
-			for i := pruneNumber + 1; i <= scanNumber; i++ {
+			// Step 2. Read Latest Node Count
+			pruneBodyStart := uint64(0)
+
+			if pruneNumber > 0 {
+
+				pruneBodyStart = pruneNumber + 1
+
+				// Add previous state root for prune
+				for i := pruneNumber + 1; i <= scanNumber; i++ {
+					header := p.bc.GetHeaderByNumber(i)
+					p.countBlockChainTrie(header.Root, false)
+					log.Infof("countBlockChainTrie for block %d", i)
+				}
+			} else {
+				pruneBodyStart = scanStart
+			}
+
+			for i := scanStart; i <= scanEnd+1; i++ {
+
+				//TODO Cache the header
 				header := p.bc.GetHeaderByNumber(i)
-				stateRoots = append(stateRoots, header.Root)
+				p.countBlockChainTrie(header.Root, false)
+
+				//log.Printf("Block: %v, Root %x", i, header.Root)
+				if i%max_count_trie == max_count_trie-1 || i == scanEnd {
+
+					p.bc.MuLock()
+
+					header := p.bc.CurrentBlock().Header()
+					log.Infof("lastest block number is %v\n", header.Number.Uint64())
+					p.countBlockChainTrie(header.Root, true)
+					p.processScanData(i)
+
+					p.bc.MuUnLock()
+
+					if p.pruneBodyData {
+						for j := pruneBodyStart; j <= i; j++ {
+							rawdb.DeleteBody(p.chainDb, rawdb.ReadCanonicalHash(p.chainDb, i), i)
+						}
+						log.Infof("deleted block from %v to %v", pruneBodyStart, i-1)
+						pruneBodyStart = i
+					}
+				}
+				//log.Infof("countBlockChainTrie for block %d", i)
 			}
-		}
 
-		for i := scanStart; i <= scanEnd; i++ {
+			blockNumber = p.bc.CurrentBlock().NumberU64()
+			scanNumber = scanEnd + 1
+			pruneNumber = scanEnd
 
-			//TODO Cache the header
-			header := p.bc.GetHeaderByNumber(i)
-
-			//log.Printf("Block: %v, Root %x", i, header.Root)
-			if skip := p.countBlockChainTrie(header.Root, nodeCount); skip {
-				newScanNumber, newPruneNumber = i, i
-				continue
-			}
-
-			stateRoots = append(stateRoots, header.Root)
-
-			// Prune Data Process
-			if len(nodeCount) > 0 && len(stateRoots) >= int(max_count_trie) {
-				newScanNumber = i
-				newPruneNumber = p.processScanData(newScanNumber, stateRoots, nodeCount)
-				// Remain state root
-				stateRoots = stateRoots[len(stateRoots)-int(max_remain_trie):]
-			}
-		}
-
-		// Process the remaining state root
-		if len(nodeCount) > 0 {
-			newScanNumber = scanEnd
-			newPruneNumber = p.processScanData(newScanNumber, stateRoots, nodeCount)
-		} else if newScanNumber > scanNumber || newPruneNumber > pruneNumber {
-			// Update the last number if skip case happened
-			p.writeLastNumber(newScanNumber, newPruneNumber)
+		} else {
+			time.Sleep(5 * time.Second) //sleep 5 seconds to wait more blocks to prune
+			blockNumber = p.bc.CurrentBlock().NumberU64()
 		}
 	}
-	return newScanNumber, newPruneNumber
+
+	return scanEnd+1, scanEnd
 }
 
 func calculateScan(scan, latestBlockHeight uint64) (scanOrNot bool, from, to uint64) {
 
-	var unscanHeight uint64
-	if scan == 0 {
-		unscanHeight = latestBlockHeight
-		from = 0
-	} else {
-		unscanHeight = latestBlockHeight - scan
-		from = scan + 1
-	}
+	from = scan
+	to = 0
 
-	if unscanHeight > max_count_trie {
-		mul := latestBlockHeight / max_count_trie
-		to = mul * max_count_trie
+	unscanHeight := latestBlockHeight - scan
+	if unscanHeight > max_remain_trie {
+		to = latestBlockHeight - max_remain_trie
 	}
 
 	if to != 0 {
@@ -164,7 +178,7 @@ func (p *PruneProcessor) readLatestNodeCount(scanNumber, pruneNumber uint64) Nod
 	return nodeCount
 }
 
-func (p *PruneProcessor) countBlockChainTrie(root common.Hash, nodeCount NodeCount) (skip bool) {
+func (p *PruneProcessor) countBlockChainTrie(root common.Hash, markNoPrune bool) (skip bool) {
 	t, openErr := p.bc.StateCache().OpenTrie(root)
 	if openErr != nil {
 		if _, ok := openErr.(*trie.MissingNodeError); ok {
@@ -176,10 +190,10 @@ func (p *PruneProcessor) countBlockChainTrie(root common.Hash, nodeCount NodeCou
 		return
 	}
 
-	countTrie(t, nodeCount, func(addr common.Address, account state.Account) {
+	countTrie(t, p.nodeCount, markNoPrune, func(addr common.Address, account state.Account) {
 		if account.Root != emptyRoot {
 			if storageTrie, stErr := p.bc.StateCache().OpenStorageTrie(common.Hash{}, account.Root); stErr == nil {
-				countTrie(storageTrie, nodeCount, nil)
+				countTrie(storageTrie, p.nodeCount, markNoPrune, nil)
 			} else {
 				log.Error("Data Reduction - Error when open the Storage Trie", "err", stErr, "storageroot", account.Root, "account", addr)
 			}
@@ -187,7 +201,7 @@ func (p *PruneProcessor) countBlockChainTrie(root common.Hash, nodeCount NodeCou
 
 		if account.TX1Root != emptyRoot {
 			if tx1Trie, tx1Err := p.bc.StateCache().OpenTX1Trie(common.Hash{}, account.TX1Root); tx1Err == nil {
-				countTrie(tx1Trie, nodeCount, nil)
+				countTrie(tx1Trie, p.nodeCount, markNoPrune, nil)
 			} else {
 				log.Error("Data Reduction - Error when open the TX1 Trie", "err", tx1Err, "tx1root", account.TX1Root, "account", addr)
 			}
@@ -195,7 +209,7 @@ func (p *PruneProcessor) countBlockChainTrie(root common.Hash, nodeCount NodeCou
 
 		if account.TX3Root != emptyRoot {
 			if tx3Trie, tx3Err := p.bc.StateCache().OpenTX3Trie(common.Hash{}, account.TX3Root); tx3Err == nil {
-				countTrie(tx3Trie, nodeCount, nil)
+				countTrie(tx3Trie, p.nodeCount, markNoPrune, nil)
 			} else {
 				log.Error("Data Reduction - Error when open the TX3 Trie", "err", tx3Err, "tx3root", account.TX3Root, "account", addr)
 			}
@@ -203,7 +217,7 @@ func (p *PruneProcessor) countBlockChainTrie(root common.Hash, nodeCount NodeCou
 
 		if account.ProxiedRoot != emptyRoot {
 			if proxiedTrie, proxiedErr := p.bc.StateCache().OpenProxiedTrie(common.Hash{}, account.ProxiedRoot); proxiedErr == nil {
-				countTrie(proxiedTrie, nodeCount, nil)
+				countTrie(proxiedTrie, p.nodeCount, markNoPrune, nil)
 			} else {
 				log.Error("Data Reduction - Error when open the Proxied Trie", "err", proxiedErr, "proxiedroot", account.ProxiedRoot, "account", addr)
 			}
@@ -211,7 +225,7 @@ func (p *PruneProcessor) countBlockChainTrie(root common.Hash, nodeCount NodeCou
 
 		if account.RewardRoot != emptyRoot {
 			if rewardTrie, rewardErr := p.bc.StateCache().OpenRewardTrie(common.Hash{}, account.RewardRoot); rewardErr == nil {
-				countTrie(rewardTrie, nodeCount, nil)
+				countTrie(rewardTrie, p.nodeCount, markNoPrune, nil)
 			} else {
 				log.Error("Data Reduction - Error when open the Reward Trie", "err", rewardErr, "rewardroot", account.RewardRoot, "account", addr)
 			}
@@ -220,73 +234,132 @@ func (p *PruneProcessor) countBlockChainTrie(root common.Hash, nodeCount NodeCou
 	return
 }
 
-func countTrie(t state.Trie, nodeCount NodeCount, processLeaf processLeafTrie) {
-	child := true
-	for it := t.NodeIterator(nil); it.Next(child); {
-		if !it.Leaf() {
-			nodeHash := it.Hash()
-			if _, exist := nodeCount[nodeHash]; exist {
-				nodeCount[nodeHash]++
-				child = false
-			} else {
-				nodeCount[nodeHash] = 1
-				child = true
-			}
-		} else {
-			// Process the Account -> Inner Trie
-			if processLeaf != nil {
-				addr := t.GetKey(it.LeafKey())
-				if len(addr) == 20 {
-					var data state.Account
-					rlp.DecodeBytes(it.LeafBlob(), &data)
+func countTrie(t state.Trie, nodeCount NodeCount, markNoPrune bool, processLeaf processLeafTrie) {
 
-					processLeaf(common.BytesToAddress(addr), data)
+	child := true
+	if !markNoPrune {
+		for it := t.NodeIterator(nil); it.Next(child); {
+			if !it.Leaf() {
+				nodeHash := it.Hash()
+				if _, exist := nodeCount[nodeHash]; exist {
+					child = false
+				} else {
+					nodeCount[nodeHash] = 0 //this node occurs, may need prune
+					child = true
+				}
+			} else {
+				// Process the Account -> Inner Trie
+				if processLeaf != nil {
+					addr := t.GetKey(it.LeafKey())
+					if len(addr) == 20 {
+						var data state.Account
+						rlp.DecodeBytes(it.LeafBlob(), &data)
+
+						processLeaf(common.BytesToAddress(addr), data)
+					}
+				}
+			}
+		}
+	} else {
+		for it := t.NodeIterator(nil); it.Next(child); {
+			if !it.Leaf() {
+				nodeHash := it.Hash()
+				nodeCount[nodeHash] = 1 //this node occurs in the latest block, mark no prune
+			} else {
+				// Process the Account -> Inner Trie
+				if processLeaf != nil {
+					addr := t.GetKey(it.LeafKey())
+					if len(addr) == 20 {
+						var data state.Account
+						rlp.DecodeBytes(it.LeafBlob(), &data)
+
+						processLeaf(common.BytesToAddress(addr), data)
+					}
 				}
 			}
 		}
 	}
 }
 
-func (p *PruneProcessor) processScanData(latestScanNumber uint64, stateRoots []common.Hash, nodeCount NodeCount) uint64 {
-	log.Infof("Data Reduction - After Scan, Total Nodes: %d, %d", latestScanNumber, len(nodeCount))
+func (p *PruneProcessor) processScanData(latestScanNumber uint64) uint64 {
+
+	log.Infof("Data Reduction - After Scan, lastest scan number: %d", latestScanNumber)
 
 	// Prune State Data
-	p.pruneData(stateRoots[:len(stateRoots)-int(max_remain_trie)], nodeCount)
+	p.pruneData()
 
-	newPruneNumber := latestScanNumber - max_remain_trie
+	newPruneNumber := latestScanNumber
 
 	// Commit the new scaned/pruned node count to trie
-	p.commitDataPruneTrie(nodeCount, latestScanNumber, newPruneNumber)
+	p.writeLastNumber(latestScanNumber, newPruneNumber)
 
-	log.Infof("Data Reduction - After Prune, Total Nodes: %d", len(nodeCount))
 	log.Infof("Data Reduction - Scan/Prune Completed for trie %d %d", latestScanNumber, newPruneNumber)
 	return newPruneNumber
 }
 
-func (p *PruneProcessor) pruneData(stateRoots []common.Hash, nodeCount NodeCount) {
-	for _, root := range stateRoots {
-		p.pruneBlockChainTrie(root, nodeCount)
+func (p *PruneProcessor) pruneData() {
+
+	count := 0
+
+	batch := p.chainDb.NewBatch()
+	for node, latest := range p.nodeCount {
+		if latest == 0 {
+			if batchDeleteError := batch.Delete(node.Bytes()); batchDeleteError != nil {
+				log.Error("Data Reduction - Error when delete the hash from chaindb", "err", batchDeleteError, "hash", node)
+			}
+			delete(p.nodeCount, node)
+			count ++
+		} else {
+			p.nodeCount[node] = 0
+		}
 	}
 
-	if len(p.pendingDeleteHashList) > 0 {
-		log.Infof("Data Reduction - %d hashes will be deleted from chaindb", len(p.pendingDeleteHashList))
-		batch := p.chainDb.NewBatch()
-		for _, hash := range p.pendingDeleteHashList {
-			if batchDeleteError := batch.Delete(hash.Bytes()); batchDeleteError != nil {
-				log.Error("Data Reduction - Error when delete the hash from chaindb", "err", batchDeleteError, "hash", hash)
-			}
-		}
-		if writeErr := batch.Write(); writeErr != nil {
-			log.Error("Data Reduction - Error when write the deletion batch", "err", writeErr)
-		} else {
-			log.Info("Data Reduction - write the deletion batch success")
-		}
-		p.pendingDeleteHashList = nil
+	log.Infof("Data Reduction - %d hashes will be deleted from chaindb", count)
+	if writeErr := batch.Write(); writeErr != nil {
+		log.Error("Data Reduction - Error when write the deletion batch", "err", writeErr)
 	} else {
-		log.Info("Data Reduction - no hashes will be deleted from chaindb")
+		log.Infof("Data Reduction - write the deletion batch success, delete %v hashes", count)
 	}
 }
 
+func (p *PruneProcessor) writeLastNumber(lastScanNumber, lastPruneNumber uint64) {
+	rawdb.WriteHeadScanNumber(p.db, lastScanNumber)
+	rawdb.WriteHeadPruneNumber(p.db, lastPruneNumber)
+}
+
+func (nc NodeCount) String() string {
+	list := make([]common.Hash, 0, len(nc))
+	for key := range nc {
+		list = append(list, key)
+	}
+	sort.Slice(list, func(i, j int) bool {
+		return bytes.Compare(list[i].Bytes(), list[j].Bytes()) == 1
+	})
+
+	result := ""
+	for _, key := range list {
+		result += fmt.Sprintf("%v: %d \n", key.Hex(), nc[key])
+	}
+	return result
+}
+
+func GetLatestStatus(prunedb ethdb.Database) *PruneStatus {
+	var scanNo, pruneNo uint64
+	if ps := rawdb.ReadHeadScanNumber(prunedb); ps != nil {
+		scanNo = *ps
+	}
+	if pp := rawdb.ReadHeadPruneNumber(prunedb); pp != nil {
+		pruneNo = *pp
+	}
+
+	return &PruneStatus{
+		Running:           atomic.LoadInt32(&pruning) == 1,
+		LatestScanNumber:  scanNo,
+		LatestPruneNumber: pruneNo,
+	}
+}
+
+/*
 func (p *PruneProcessor) pruneBlockChainTrie(root common.Hash, nodeCount NodeCount) {
 	t, openErr := p.bc.StateCache().OpenTrie(root)
 	if openErr != nil {
@@ -389,40 +462,4 @@ func (p *PruneProcessor) commitDataPruneTrie(nodeCount NodeCount, lastScanNumber
 	// Write the last number
 	p.writeLastNumber(lastScanNumber, lastPruneNumber)
 }
-
-func (p *PruneProcessor) writeLastNumber(lastScanNumber, lastPruneNumber uint64) {
-	rawdb.WriteHeadScanNumber(p.db, lastScanNumber)
-	rawdb.WriteHeadPruneNumber(p.db, lastPruneNumber)
-}
-
-func (nc NodeCount) String() string {
-	list := make([]common.Hash, 0, len(nc))
-	for key := range nc {
-		list = append(list, key)
-	}
-	sort.Slice(list, func(i, j int) bool {
-		return bytes.Compare(list[i].Bytes(), list[j].Bytes()) == 1
-	})
-
-	result := ""
-	for _, key := range list {
-		result += fmt.Sprintf("%v: %d \n", key.Hex(), nc[key])
-	}
-	return result
-}
-
-func GetLatestStatus(prunedb ethdb.Database) *PruneStatus {
-	var scanNo, pruneNo uint64
-	if ps := rawdb.ReadHeadScanNumber(prunedb); ps != nil {
-		scanNo = *ps
-	}
-	if pp := rawdb.ReadHeadPruneNumber(prunedb); pp != nil {
-		pruneNo = *pp
-	}
-
-	return &PruneStatus{
-		Running:           atomic.LoadInt32(&pruning) == 1,
-		LatestScanNumber:  scanNo,
-		LatestPruneNumber: pruneNo,
-	}
-}
+*/
