9efc19a58aa60eb37f5191cbcc4451609d8de188
pchain-org <lv@pchain.org>
2019-08-29 18:21:10 +0800

    make block generation time to 2 seconds to release disk usage temporarily

--- vendor/github.com/ethereum/go-ethereum/consensus/tendermint/config/tendermint/config.go
+++ vendor/github.com/ethereum/go-ethereum/consensus/tendermint/config/tendermint/config.go
@@ -105,8 +105,7 @@ func GetConfig(rootDir, chainId string) cfg.Config {
 	mapConfig.SetDefault("timeout_prevote_delta", 500)
 	mapConfig.SetDefault("timeout_precommit", 2000)
 	mapConfig.SetDefault("timeout_precommit_delta", 500)
-	mapConfig.SetDefault("timeout_commit", 100)
-	//mapConfig.SetDefault("timeout_commit", 1000)
+	mapConfig.SetDefault("timeout_commit", 2000)
 
 	// make progress asap (no `timeout_commit`) on full precommit votes
 	mapConfig.SetDefault("skip_timeout_commit", false)
--- vendor/github.com/ethereum/go-ethereum/consensus/tendermint/consensus/state1.go
+++ vendor/github.com/ethereum/go-ethereum/consensus/tendermint/consensus/state1.go
@@ -106,8 +106,7 @@ func (cs *ConsensusState) UpdateToState(state *sm.State) {
 
 	// RoundState fields
 	cs.updateRoundStep(0, RoundStepNewHeight)
-	cs.StartTime = cs.timeoutParams.Commit(time.Now())
-	/*
+	//cs.StartTime = cs.timeoutParams.Commit(time.Now())
 	if cs.CommitTime.IsZero() {
 		// "Now" makes it easier to sync up dev nodes.
 		// We add timeoutCommit to allow transactions
@@ -118,8 +117,7 @@ func (cs *ConsensusState) UpdateToState(state *sm.State) {
 	} else {
 		cs.StartTime = cs.timeoutParams.Commit(cs.CommitTime)
 	}
-	*/
-	
+
 	// Reset fields based on state.
 	_, validators, _ := state.GetValidators()
 	cs.Validators = validators
