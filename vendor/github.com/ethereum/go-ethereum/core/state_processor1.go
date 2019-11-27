aa14b442f8b9c15366ccbdbdd076cc4fa0c1f7f4
pchain-org <lv@pchain.org>
2019-11-27 17:12:47 +0800

    refactor the code and make 'status' field in Receipt correct after this fork

--- vendor/github.com/ethereum/go-ethereum/core/state_processor1.go
+++ vendor/github.com/ethereum/go-ethereum/core/state_processor1.go
@@ -3,7 +3,6 @@ package core
 import (
 	"fmt"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/consensus"
 	"github.com/ethereum/go-ethereum/core/state"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
