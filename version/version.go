7b012cf6c404d252fa486d236db1e41387990b94
liaoyd <37823194+liaoyd@users.noreply.github.com>
2020-04-11 17:11:15 +0800

    update version to 1.3.1

--- version/version.go
+++ version/version.go
@@ -7,7 +7,7 @@ import (
 const (
 	Major = 1  // Major version component of the current release
 	Minor = 3  // Minor version component of the current release
-	Patch = 0  // Patch version component of the current release
+	Patch = 1  // Patch version component of the current release
 	Meta  = "" // Version metadata to append to the version string
 )
 
