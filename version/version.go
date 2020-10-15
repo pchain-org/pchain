8b9cba3ef5364acdf8a1f69a368b1f3231972670
clyde <cybaoz@163.com>
2020-10-15 10:15:00 +0800

    update version to 1.3.5

--- version/version.go
+++ version/version.go
@@ -7,7 +7,7 @@ import (
 const (
 	Major = 1  // Major version component of the current release
 	Minor = 3  // Minor version component of the current release
-	Patch = 4  // Patch version component of the current release
+	Patch = 5  // Patch version component of the current release
 	Meta  = "" // Version metadata to append to the version string
 )
 
