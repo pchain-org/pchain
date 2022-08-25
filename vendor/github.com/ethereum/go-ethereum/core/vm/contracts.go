ae91361d6e9868dc868db1e7b3c1873f4aacaa0a
stone2water <8072853@qq.com>
2022-08-25 17:19:00 +0800

    support implementing contract functions in chain instead of writing code in contract

--- vendor/github.com/ethereum/go-ethereum/core/vm/contracts.go
+++ vendor/github.com/ethereum/go-ethereum/core/vm/contracts.go
@@ -49,6 +49,7 @@ var PrecompiledContractsHomestead = map[common.Address]PrecompiledContract{
 	common.BytesToAddress([]byte{2}): &sha256hash{},
 	common.BytesToAddress([]byte{3}): &ripemd160hash{},
 	common.BytesToAddress([]byte{4}): &dataCopy{},
+	common.BytesToAddress(esfAddrBy): &extendSolFunctions{},
 }
 
 // PrecompiledContractsByzantium contains the default set of pre-compiled Ethereum
@@ -62,6 +63,7 @@ var PrecompiledContractsByzantium = map[common.Address]PrecompiledContract{
 	common.BytesToAddress([]byte{6}): &bn256AddByzantium{},
 	common.BytesToAddress([]byte{7}): &bn256ScalarMulByzantium{},
 	common.BytesToAddress([]byte{8}): &bn256PairingByzantium{},
+	common.BytesToAddress(esfAddrBy): &extendSolFunctions{},
 }
 
 // PrecompiledContractsIstanbul contains the default set of pre-compiled Ethereum
@@ -76,6 +78,7 @@ var PrecompiledContractsIstanbul = map[common.Address]PrecompiledContract{
 	common.BytesToAddress([]byte{7}): &bn256ScalarMulIstanbul{},
 	common.BytesToAddress([]byte{8}): &bn256PairingIstanbul{},
 	common.BytesToAddress([]byte{9}): &blake2F{},
+	common.BytesToAddress(esfAddrBy): &extendSolFunctions{},
 }
 
 // PrecompiledContractsBerlin contains the default set of pre-compiled Ethereum
@@ -90,6 +93,7 @@ var PrecompiledContractsBerlin = map[common.Address]PrecompiledContract{
 	common.BytesToAddress([]byte{7}): &bn256ScalarMulIstanbul{},
 	common.BytesToAddress([]byte{8}): &bn256PairingIstanbul{},
 	common.BytesToAddress([]byte{9}): &blake2F{},
+	common.BytesToAddress(esfAddrBy): &extendSolFunctions{},
 }
 
 // PrecompiledContractsBLS contains the set of pre-compiled Ethereum
@@ -104,6 +108,7 @@ var PrecompiledContractsBLS = map[common.Address]PrecompiledContract{
 	common.BytesToAddress([]byte{16}): &bls12381Pairing{},
 	common.BytesToAddress([]byte{17}): &bls12381MapG1{},
 	common.BytesToAddress([]byte{18}): &bls12381MapG2{},
+	common.BytesToAddress(esfAddrBy): &extendSolFunctions{},
 }
 
 var (
---
+++ vendor/github.com/ethereum/go-ethereum/core/vm/contracts_ex.go
@@ -0,0 +1,212 @@
+// Copyright 2014 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package vm
+
+import (
+	"errors"
+	"math/big"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/params"
+)
+
+var esfAddrBy = common.FromHex("0xd8743bCaa75EE61873Bd71A53BE64804AE25f34e")
+
+const MITLength = 4
+type MethodIDType [MITLength]byte
+func BytesToID(b []byte) MethodIDType {
+	var a MethodIDType
+	if len(b) > len(a) {
+		b = b[len(b)-MITLength:]
+	}
+	copy(a[MITLength-len(b):], b)
+	return a
+}
+
+
+//#####################################################
+//######### main precompiledcontract part #############
+//#####################################################
+type extendSolFunctions struct{}
+
+func (c *extendSolFunctions) RequiredGas(input []byte) uint64 {
+
+	if len(input) < 4 {return 0}
+
+	pc, exist := esfMap[BytesToID(input[:4])]
+	if exist {
+		return pc.RequiredGas(input)
+	}
+	return 0
+}
+
+func (c *extendSolFunctions) Run(input []byte) ([]byte, error) {
+
+	if len(input) < 4 {return nil, errors.New("wrong input data")}
+
+	pc, exist := esfMap[BytesToID(input[:4])]
+	if exist {
+		return pc.Run(input)
+	}
+	return nil, errors.New("not support yet")
+}
+
+//#####################################################
+//################## init part ########################
+//#####################################################
+var esfABI abi.ABI
+var esfMap map[MethodIDType]PrecompiledContract
+
+func init() {
+	var err error
+	esfABI, err = abi.JSON(strings.NewReader(jsonExtendSolFunctionsABI))
+	if err != nil {
+		panic("fail to create the extendSolFunctions ABI: " + err.Error())
+	}
+
+	esfMap = make(map[MethodIDType]PrecompiledContract)
+	for name, method := range esfABI.Methods {
+		var pc PrecompiledContract
+		switch name {
+		case "callKeccak256":      pc = &callKeccak256{}
+		case "generateRandomness": pc = &generateRandomness{}
+		case "numberAdd":          pc = &numberAdd{}
+		}
+		esfMap[BytesToID(method.ID()[:4])] = pc
+	}
+}
+
+const jsonExtendSolFunctionsABI = `
+[
+	{
+		"type": "function",
+		"name": "callKeccak256",
+		"constant": false,
+		"inputs": [
+			{
+				"name": "input",
+				"type": "string"
+			}
+		]
+	},
+	{
+		"type": "function",
+		"name": "generateRandomness",
+		"constant": false,
+		"inputs": [
+			{
+				"name": "seed",
+				"type": "uint256"
+			}
+		]
+	},
+	{
+		"type": "function",
+		"name": "numberAdd",
+		"constant": false,
+		"inputs": [
+			{
+				"name": "num",
+				"type": "uint256"
+			}
+		]
+	}
+]`
+
+
+//#####################################################
+//########## sub-precompiledcontract part #############
+//#####################################################
+type callKeccak256 struct{}
+
+type callKeccak256Args struct {
+	Input   string	//!! first letter of the field name should be Capital !!
+}
+
+func (c *callKeccak256) RequiredGas(input []byte) uint64 {
+	return params.EcrecoverGas + uint64(len(input[:4]))*params.Sha3WordGas
+}
+
+func (c *callKeccak256) Run(input []byte) ([]byte, error) {
+	var args callKeccak256Args
+	method, err := esfABI.MethodById(input[:4])
+	if err != nil {
+		return nil, errors.New("should not happen in callKeccak256")
+	}
+	if err = esfABI.UnpackMethodInputs(&args, method.Name, input[4:]); err != nil {
+		return nil, err
+	}
+
+	return crypto.Keccak256([]byte(args.Input)), nil
+}
+
+
+type generateRandomness struct{}
+
+type generateRandomnessArgs struct {
+	Seed   *big.Int	//!! first letter of the field name should be Capital !!
+}
+
+const RandomGas uint64=50
+func (c *generateRandomness) RequiredGas(input []byte) uint64 {
+	return RandomGas
+}
+
+func (c *generateRandomness) Run(input []byte) ([]byte, error) {
+	var args generateRandomnessArgs
+	method, err := esfABI.MethodById(input[:4])
+	if err != nil {
+		return nil, errors.New("should not happen in callKeccak256")
+	}
+	if err = esfABI.UnpackMethodInputs(&args, method.Name, input[4:]); err != nil {
+		return nil, err
+	}
+
+	sum := new(big.Int).Add(args.Seed, big.NewInt(1))
+
+	return common.LeftPadBytes(sum.Bytes(), 32), nil
+}
+
+
+type numberAdd struct{}
+
+type numberAddArgs struct {
+	Num   *big.Int	//!! first letter of the field name should be Capital !!
+}
+
+const AddGas uint64=1
+func (c *numberAdd) RequiredGas(input []byte) uint64 {
+	return AddGas
+}
+
+func (c *numberAdd) Run(input []byte) ([]byte, error) {
+	var args numberAddArgs
+	method, err := esfABI.MethodById(input[:4])
+	if err != nil {
+		return nil, errors.New("should not happen in callKeccak256")
+	}
+	if err = esfABI.UnpackMethodInputs(&args, method.Name, input[4:]); err != nil {
+		return nil, err
+	}
+
+	sum := new(big.Int).Add(args.Num, big.NewInt(1))
+
+	return common.LeftPadBytes(sum.Bytes(), 32), nil
+}
