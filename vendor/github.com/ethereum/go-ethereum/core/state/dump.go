277cf4674a42e929990833a35fcf311a029d5aab
Terry108 <996240986@qq.com>
2024-07-03 09:09:32 +0800

    update export format

--- vendor/github.com/ethereum/go-ethereum/core/state/dump.go
+++ vendor/github.com/ethereum/go-ethereum/core/state/dump.go
@@ -24,10 +24,10 @@ import (
 	"io/ioutil"
 	"math/big"
 	"os"
-	"strconv"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/rlp"
@@ -365,21 +365,45 @@ func (self *StateDB) RawDumpBalanceToFile(height uint64, filename string) {
 			if err := rlp.DecodeBytes(it.Value, &data); err != nil {
 				panic(err)
 			}
-			pi := big.NewInt(params.PI)
+			pi := big.NewInt(params.GWei)
 			if data.Balance.Sign() != 0 || data.DelegateBalance.Sign() != 0 || data.DepositBalance.Sign() != 0 {
 				var acc []string
-				acc = append(acc,
-					"0x"+common.Bytes2Hex(addr),
-					data.Balance.Div(data.Balance, pi).String(),
-					data.DepositBalance.Div(data.DepositBalance, pi).String(),
-					data.DelegateBalance.Div(data.DelegateBalance, pi).String(),
-					data.ProxiedBalance.Div(data.ProxiedBalance, pi).String(),
-					data.DepositProxiedBalance.Div(data.DepositProxiedBalance, pi).String(),
-					data.PendingRefundBalance.Div(data.PendingRefundBalance, pi).String(),
-					data.RewardBalance.Div(data.RewardBalance, pi).String(),
-					strconv.FormatUint(data.Nonce, 10),
-				)
-				accounts = append(accounts, acc)
+				// acc = append(acc,
+				// 	"0x"+common.Bytes2Hex(addr),
+				// 	data.Balance.Div(data.Balance, pi).String(),
+				// 	data.DepositBalance.Div(data.DepositBalance, pi).String(),
+				// 	data.DelegateBalance.Div(data.DelegateBalance, pi).String(),
+				// 	data.ProxiedBalance.Div(data.ProxiedBalance, pi).String(),
+				// 	data.DepositProxiedBalance.Div(data.DepositProxiedBalance, pi).String(),
+				// 	data.PendingRefundBalance.Div(data.PendingRefundBalance, pi).String(),
+				// 	data.RewardBalance.Div(data.RewardBalance, pi).String(),
+				// 	strconv.FormatUint(data.Nonce, 10),
+				// )
+
+				// totalBalance = Balance + DepositBalance + DelegateBalance + PendingRefundBalance + RewardBalance
+				totalBalance := data.Balance
+				if data.DepositBalance.Sign() != 0 {
+					totalBalance.Add(totalBalance, data.DepositBalance)
+				}
+				if data.DelegateBalance.Sign() != 0 {
+					totalBalance.Add(totalBalance, data.DelegateBalance)
+				}
+				if data.PendingRefundBalance.Sign() != 0 {
+					totalBalance.Add(totalBalance, data.PendingRefundBalance)
+				}
+				if data.RewardBalance.Sign() != 0 {
+					totalBalance.Add(totalBalance, data.RewardBalance)
+				}
+				if totalBalance.Sign() != 0 {
+					fb, _ := totalBalance.Div(data.Balance, pi).Float64()
+					fb = fb / 1e9
+					acc = append(acc,
+						"0x"+common.Bytes2Hex(addr),
+						fmt.Sprintf("%s", fb),
+						(*hexutil.Big)(totalBalance).String(),
+					)
+					accounts = append(accounts, acc)
+				}
 			}
 		}
 	}
@@ -391,7 +415,8 @@ func (self *StateDB) RawDumpBalanceToFile(height uint64, filename string) {
 	defer file.Close()
 
 	writer := csv.NewWriter(file)
-	writer.Write([]string{"adress", "balance", "deposit_balance", "delegate_balance", "proxied_balance", "deposit_proxied_balance", "pending_refund_balance", "reward_balance", "nonce"})
+	// writer.Write([]string{"address", "balance", "deposit_balance", "delegate_balance", "proxied_balance", "deposit_proxied_balance", "pending_refund_balance", "reward_balance", "nonce"})
+	writer.Write([]string{"address", "balance", "hex_balance"})
 	defer writer.Flush()
 
 	for _, acc := range accounts {
